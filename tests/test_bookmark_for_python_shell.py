2e05af6a04249f9907da4b5e2814dd8b85af7d05
Srinivasarao Daruna <daruns@amazon.com>
2022-01-14 12:30:33 -0500

    Improve the tests coverage
    Adding test cases for additional file types load and exception handling.
    
    This will bring the test percentage to 100.

--- tests/test_bookmark_for_python_shell.py
+++ tests/test_bookmark_for_python_shell.py
@@ -57,14 +57,16 @@ class TestBookmark:
         # upload initial files
         for fname in os.listdir(os.path.join(dir_here, "data")):
             path = os.path.join(dir_here, "data", fname)
-            key = f"{cls.test_s3_prefix}/a.csv"
+            file_type = fname.split(".")[-1]
+            key = f"{cls.test_s3_prefix}_{file_type}/{fname}"
             s3.upload_file(path, Bucket=cls.test_s3_bucket, Key=key)
 
     # --- Test cases
     def test_load_data_from_s3(self):
+        s3_path_test = self.test_s3_prefix + "_csv"
         data_loader = DataLoader(
             s3_bucket_name=self.test_s3_bucket,
-            s3_location=self.test_s3_prefix,
+            s3_location=s3_path_test,
             format_of_data="csv",
             job_name="job_123",
             dynamo_db_table_for_bookmark_storage=self.test_dynamodb_table,
@@ -72,9 +74,67 @@ class TestBookmark:
 
         data_loader.load_data_from_s3()
 
+    def test_parquet_data_load(self):
+        s3_path_test = self.test_s3_prefix + "_parquet"
+        data_loader = DataLoader(
+            s3_bucket_name=self.test_s3_bucket,
+            s3_location=s3_path_test,
+            format_of_data="parquet",
+            job_name="job_test_parquet_data_load",
+            dynamo_db_table_for_bookmark_storage=self.test_dynamodb_table)
+
+        df = data_loader.load_data_from_s3()
+        assert df.shape[0] != 0
+
+    def test_json_data_load(self):
+        s3_path_test = self.test_s3_prefix + "_json"
+        data_loader = DataLoader(
+            s3_bucket_name=self.test_s3_bucket,
+            s3_location=s3_path_test,
+            format_of_data="json",
+            job_name="job_test_parquet_data_load",
+            dynamo_db_table_for_bookmark_storage=self.test_dynamodb_table)
+
+        df = data_loader.load_data_from_s3()
+        assert df.shape[0] != 0
+
+    def test_commit_and_load(self):
+        data_loader = DataLoader(
+            s3_bucket_name="bucket-for-datalab",
+            s3_location="folder-for-bookmark-test",
+            format_of_data="csv",
+            job_name="job_123",
+            dynamo_db_table_for_bookmark_storage=self.test_dynamodb_table)
+
+        load1 = data_loader.load_data_from_s3()
+        load2 = data_loader.load_data_from_s3()
+        assert load1.shape[0] != 0 and load2.shape[0] != 0
+        data_loader.commit()
+        data_after_commit = data_loader.load_data_from_s3()
+        assert data_after_commit.shape[0] == 0
+        data_loader.load_data_from_s3()
+
+    def test_invalid_file_type_exception(self):
+        with pytest.raises(Exception) as ex:
+            DataLoader(
+                "bucket-for-datalab",
+                "folder-for-bookmark-test",
+                "avro",
+                "job_123"
+                "bookmark_table")
+
+    def clean_up_dynamo_table(self):
+        dynamodb_client = boto3.client("dynamodb")
+        try:
+            dynamodb_client.describe_table(TableName=self.test_dynamodb_table)
+            dynamodb_client.delete_table(TableName=self.test_dynamodb_table)
+        except dynamodb_client.exceptions.ResourceNotFoundException:
+            pass
 
 if __name__ == "__main__":
     import os
 
+    print("started from main")
     basename = os.path.basename(__file__)
     pytest.main([basename, "-s", "--tb=native"])
+
